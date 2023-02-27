cbd9b040823e5e56701b4734ca3f218e85b6f266
nidheesh <nidhin@Dilips-iMac.local>
2023-02-27 10:17:09 +0530

    sol

--- src/main/java/com/driver/services/impl/DriverServiceImpl.java
+++ src/main/java/com/driver/services/impl/DriverServiceImpl.java
@@ -36,8 +36,10 @@ public class DriverServiceImpl implements DriverService {
 	public void removeDriver(int driverId) {
 		// Delete driver without using deleteById function
 
+       Driver driver=driverRepository3.findById(driverId).get();
+	   driverRepository3.delete(driver);
 
-		 driverRepository3.deleteById(driverId);
+
 	}
 
 	@Override
